1bf22672efcec960b2e08cae3b0a4371c6bd1537
Johann Schulenburg <56830175+JohannSchulenburg@users.noreply.github.com>
2024-04-30 09:19:02 +0200

    NPC Scene & easy npc creation

--- GodotProject/Scripts/NPC.gd
+++ GodotProject/Scripts/NPC.gd
@@ -3,8 +3,13 @@ extends Node
 class_name NPC
 
 @export var id: int
+var associated_llm: String
+#Called when object is initializing
+func _init(given_name:="Jeff", given__llm:="phi"): 
+	name = given_name
+	associated_llm = given__llm
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#assert(id != null, "WARNING: NPC instance has no id!") this can be used to debug (basically, if then print)
 	id = NPCManager.npc_list.size()
-	NPCManager.add_npc_to_list(self)
--- GodotProject/Scripts/NPCManager.gd
+++ GodotProject/Scripts/NPCManager.gd
@@ -3,21 +3,23 @@ extends Node
 class_name NPCManager
 
 static var npc_list: Array = []
-
-static func add_npc_to_list(_npc: NPC):
-	npc_list.append(_npc)
+static var npc_tree_scene: PackedScene
 
 static func get_npc_by_id(_id: int):
 	for i in range(npc_list.size()):
 		if(npc_list[i].id == _id):
 			return npc_list[i]
 
+func create_npc(_given_name: String, _given_llm: String):
+	var _npc: NPC = npc_tree_scene.instantiate()
+	_npc.name = _given_name
+	_npc.associated_llm = _given_llm
+	npc_list.append(_npc)
+	self.add_child(_npc)
+
 func _ready():
 	print("DEBUG: instantiating NPCs...")
-	var scene = load("res://Scenes/npc_tree.tscn")
-	var npc_tree_scene = preload("res://Scenes/npc_tree.tscn")
-	self.add_child(npc_tree_scene.instantiate())
-	self.add_child(npc_tree_scene.instantiate())
-	self.add_child(npc_tree_scene.instantiate())
+	npc_tree_scene = preload("res://Scenes/npc_tree.tscn")
+	create_npc("Elong Ma", "Starlink")
+	create_npc("bruh", "bruh")
 	print(npc_list)
-	
