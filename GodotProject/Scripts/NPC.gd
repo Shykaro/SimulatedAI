31362dc2afe08e12c11ffea3e8d3d40b91869c00
Johann Schulenburg <56830175+JohannSchulenburg@users.noreply.github.com>
2024-04-30 09:47:56 +0200

    Add timers to npcs for "random" chat triggers

--- GodotProject/Scripts/NPC.gd
+++ GodotProject/Scripts/NPC.gd
@@ -4,12 +4,19 @@ class_name NPC
 
 @export var id: int
 var associated_llm: String
-#Called when object is initializing
-func _init(given_name:="Jeff", given__llm:="phi"): 
-	name = given_name
-	associated_llm = given__llm
+var chat_interval: int
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func start():
 	#assert(id != null, "WARNING: NPC instance has no id!") this can be used to debug (basically, if then print)
 	id = NPCManager.npc_list.size()
+	var timer = Timer.new()
+	timer.autostart = true
+	timer.timeout.connect(_on_timer_timeout)
+	self.add_child(timer)
+	timer.start(chat_interval)
+	print(chat_interval)
+
+func _on_timer_timeout():
+	print("id "+str(id)+" timeout!")
+
--- GodotProject/Scripts/NPCManager.gd
+++ GodotProject/Scripts/NPCManager.gd
@@ -10,16 +10,19 @@ static func get_npc_by_id(_id: int):
 		if(npc_list[i].id == _id):
 			return npc_list[i]
 
-func create_npc(_given_name: String, _given_llm: String):
+func create_npc(_given_name: String, _given_llm: String, _given_chat_interval: int):
 	var _npc: NPC = npc_tree_scene.instantiate()
 	_npc.name = _given_name
 	_npc.associated_llm = _given_llm
+	_npc.chat_interval = _given_chat_interval
 	npc_list.append(_npc)
 	self.add_child(_npc)
+	_npc.start()
 
 func _ready():
 	print("DEBUG: instantiating NPCs...")
 	npc_tree_scene = preload("res://Scenes/npc_tree.tscn")
-	create_npc("Elong Ma", "Starlink")
-	create_npc("bruh", "bruh")
+	create_npc("Elong Ma", "Starlink",10)
+	create_npc("bruh", "bruh",3)
 	print(npc_list)
+
